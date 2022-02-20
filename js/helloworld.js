9801f21902ae91406f2328305084d97a8e77d590
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-20 11:54:56 -0600

    add helloworld.js and code, works in node not on codewars for reason unknown

---
+++ js/helloworld.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//My answer
+function hello(name) {
+    return console.log('Hello, ' + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + '!');
+}
+
+hello('jamEs');
+hello('haley');
+hello('hAlEy');
