418bfcc45ae390d4aaea3db671b1522094dfc661
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-20 15:17:47 -0600

    add categorize_new_member.js and code to complete exercise

---
+++ js/categorize_new_member.js
@@ -0,0 +1,48 @@
+"use strict";
+
+// The Western Suburbs Croquet Club has two categories of membership, Senior and Open.
+// They would like your help with an application form that will tell prospective members which category they will be placed.
+//
+// To be a senior, a member must be at least 55 years old and have a handicap greater than 7.
+// In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+//
+// Input will consist of a list of pairs. Each pair contains information for a single potential member.
+// Information consists of an integer for the person's age and an integer for the person's handicap.
+//
+// Output will consist of a list of string values (in Haskell: Open or Senior)
+// stating whether the respective member is to be placed in the senior or open category.
+//
+// Example
+// input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+// output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+let input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+
+// function openOrSenior(data){
+//     let assignments = [];
+//     data.forEach(function(pair, i) {
+//         if(pair[i][0] >= 55 && pair[i][1] > 7) {
+//             assignments.push('Senior');
+//         } else {
+//             assignments.push('Open');
+//         }
+//         })
+//     return assignments;
+// }
+//
+// openOrSenior(input);
+
+function openOrSenior(data){
+    let assignments = [];
+    data.forEach(function(pair, i) {
+        if(pair[0] >= 55 && pair[1] > 7) {
+            assignments.push('Senior');
+        } else {
+            assignments.push('Open');
+        }
+    })
+    return assignments;
+}
+
+
+console.log(openOrSenior(input));
