5fd5317dfe4c601ab5ede503c70a97823e05dd92
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-20 11:56:14 -0600

    added comment

--- js/helloworld.js
+++ js/helloworld.js
@@ -1,6 +1,6 @@
 "use strict";
 
-//My answer
+//My answer works in node and in browser, but not through Codewars. Not sure why.
 function hello(name) {
     return console.log('Hello, ' + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + '!');
 }
