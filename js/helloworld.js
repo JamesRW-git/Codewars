0a1561ecc2485068ec43226a45a858bae24538e8
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-20 12:28:22 -0600

    add comments

--- js/helloworld.js
+++ js/helloworld.js
@@ -1,13 +1,16 @@
 "use strict";
 
 //My answer works in node and in browser, but not through Codewars. Not sure why.
-// function hello(name) {
-//     if(name === '' || name == null) {
-//         return  console.log('Hello, World!');
-//     } else {
-//         return console.log('Hello, ' + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + '!');
-//     }
-// }
+//Update: It works now if I remove the console logs. I guess there was an early test case
+//for outputting 'Hello, World!' for being passed an empty string or null that I
+//was misinterpreting as failing for when an actual string was passed in
+function hello(name) {
+    if(name === '' || name == null) {
+        return  console.log('Hello, World!');
+    } else {
+        return console.log('Hello, ' + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + '!');
+    }
+}
 
 hello('jamEs');
 hello('haley');
@@ -15,12 +18,12 @@ hello('hAlEy');
 hello('');
 hello();
 
-//Tried taking the 'fixed' name and assigning it to a variae, errors out about charAt
- function hello(name) {
-     if(name !== '' && name != null){
-         let fixedName = (name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
-         return console.log('Hello, ' + fixedName + '!');
-     } else {
-         return console.log('Hello, World!');
-     }
- }
+//Alternative that I also tried when trying to figure out why tests kept failing
+//  function hello(name) {
+//      if(name !== '' && name != null){
+//          let fixedName = (name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+//          return console.log('Hello, ' + fixedName + '!');
+//      } else {
+//          return console.log('Hello, World!');
+//      }
+//  }
