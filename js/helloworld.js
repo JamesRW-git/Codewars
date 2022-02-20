42272ba56271853280e1e0849611f4c692312d48
James Woodward <jamesrobertwoodward@gmail.com>
2022-02-20 12:21:59 -0600

    added code to take a passed in  null or empty string input and return desired output

--- js/helloworld.js
+++ js/helloworld.js
@@ -1,16 +1,26 @@
 "use strict";
 
 //My answer works in node and in browser, but not through Codewars. Not sure why.
-function hello(name) {
-    return console.log('Hello, ' + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + '!');
-}
+// function hello(name) {
+//     if(name === '' || name == null) {
+//         return  console.log('Hello, World!');
+//     } else {
+//         return console.log('Hello, ' + name.charAt(0).toUpperCase() + name.slice(1).toLowerCase() + '!');
+//     }
+// }
 
 hello('jamEs');
 hello('haley');
 hello('hAlEy');
+hello('');
+hello();
 
-//Tried taking the 'fixed' name and assigning it to a variable, errors out about charAt
-function hello(name) {
-    let fixedName = (name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
-    return 'Hello, ' + fixedName + '!';
-}
+//Tried taking the 'fixed' name and assigning it to a variae, errors out about charAt
+ function hello(name) {
+     if(name !== '' && name != null){
+         let fixedName = (name.charAt(0).toUpperCase() + name.slice(1).toLowerCase());
+         return console.log('Hello, ' + fixedName + '!');
+     } else {
+         return console.log('Hello, World!');
+     }
+ }
