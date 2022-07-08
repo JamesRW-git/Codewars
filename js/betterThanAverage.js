2b875a7deed8a9ff2f949a92177df52b1be8843c
James Woodward <jamesrobertwoodward@gmail.com>
2022-07-08 13:51:08 -0700

    complete betterThanAverage exercise

---
+++ js/betterThanAverage.js
@@ -0,0 +1,8 @@
+function betterThanAverage(classPoints, yourPoints) {
+    let average = 0;
+    for (let i = 0; i < classPoints.length; i++) {
+        average += classPoints[i];
+    }
+    average = average/classPoints.length;
+    return yourPoints > average;
+}
