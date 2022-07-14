f55c7dfcc59b340f10b2e64d20ccf114682e4fdc
James Woodward <jamesrobertwoodward@gmail.com>
2022-07-13 20:02:07 -0600

    complete xesAndOes exercise

--- js/xesAndOes.js
+++ js/xesAndOes.js
@@ -1,3 +1,14 @@
 function XO(str) {
-
+    let x = 0;
+    let o = 0;
+    str.split('').forEach(function (char) {
+        if (char === 'X' || char === 'x') {
+            x++;
+        } else if (char === 'O' || char === 'o') {
+            o++;
+        }
+    })
+    return (x === o);
 }
+
+console.log(XO("xxooo"));
