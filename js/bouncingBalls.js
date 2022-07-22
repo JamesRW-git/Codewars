c5a24d9874909bbaad9cfcaab0ef500742b23736
James Woodward <jamesrobertwoodward@gmail.com>
2022-07-21 22:04:51 -0500

    complete bouncingBalls exercise

---
+++ js/bouncingBalls.js
@@ -0,0 +1,14 @@
+function bouncingBall(h,  bounce,  window) {
+    let bounces = 0;
+    if (bounce < 1 && bounce > 0 && h > 0 && h > window) {
+        do {
+            bounces++;
+            h = h * bounce;
+        } while (h > window)
+        return (bounces * 2 - 1);
+    } else {
+        return -1;
+    }
+}
+
+console.log(bouncingBall(2.0, .5, 1.0))
