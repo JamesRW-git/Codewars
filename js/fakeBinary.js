dc423dec0af49f8033c2897e1aab5de6a654c501
James Woodward <jamesrobertwoodward@gmail.com>
2022-10-04 23:17:20 -0500

    refactor

--- js/fakeBinary.js
+++ js/fakeBinary.js
@@ -1,12 +1,19 @@
 function fakeBin(x) {
    let notBinary = [];
-    x.split('').forEach(function(num) {
-       if (num >= 5) {
-          notBinary.push('1');
-       } else {
-          notBinary.push('0')
-       }
-    })
+    // x.split('').forEach(function(num) {
+    //    if (num >= 5) {
+    //       notBinary.push('1');
+    //    } else {
+    //       notBinary.push('0')
+    //    }
+    // })
+    for (let i = 0; i < x.split('').length; i++) {
+        if (x.split('')[i] >= 5) {
+            notBinary.push('1');
+        } else {
+            notBinary.push('0');
+        }
+    }
    return notBinary.join('');
 }
 
