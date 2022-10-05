404e743fa9216270ae17296e148ccb1771afeb64
James Woodward <jamesrobertwoodward@gmail.com>
2022-10-04 23:19:25 -0500

    refactor

--- js/find_average.js
+++ js/find_average.js
@@ -3,9 +3,12 @@
 function find_average(array) {
     let averaged = 0;
     if (array.length > 0){
-        array.forEach(function (number){
-            averaged += number;
-        })
+        // array.forEach(function (number){
+        //     averaged += number;
+        // })
+        for (let i = 0; i < array.length; i++) {
+            averaged += array[i]
+        }
         return averaged/array.length
     } else {
         return 0;
