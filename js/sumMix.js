61a803e00020f7980bf3a3040ee553383b0dc06e
James Woodward <jamesrobertwoodward@gmail.com>
2022-10-27 23:23:12 -0500

    refactor

--- js/sumMix.js
+++ js/sumMix.js
@@ -1,7 +1,10 @@
 function oddOrEven(x){
     let sum = 0;
-    x.forEach(function(num) {
-        sum += parseInt(num);
-    })
+    // x.forEach(function(num) {
+    //     sum += parseInt(num);
+    // })
+    for (let i = 0; i < x.length; i++) {
+        sum += parseInt(x[i]);
+    }
     return (sum % 2 === 0 ? 'even' : 'odd');
 }
